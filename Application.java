b06164668831a33ce126fa57bae92ac919c2204a
Ikonostas <Alex-jks1@mail.ru>
2022-08-21 18:25:46 +0300

    добавили "Hello World"

--- Application.java
+++ Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextLine());
+        System.out.println("Hello World");
     }
 }
